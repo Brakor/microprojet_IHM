98561af6dc36be7058e4536c0f689f3ac9055a61
YANN PASCOET <pascoet@salle235-02.arda>
2017-10-12 09:39:13 +0000

    Ajout d'une fonction pour modifier les contraintes du GridBagLayout

--- sources/Projet_v1.java
+++ sources/Projet_v1.java
@@ -3,81 +3,75 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Fenetre extends JFrame{
-    public Fenetre(){
-	this.setSize(1200,800);
-        this.setLocation(0,10);
-	this.setTitle("Recherche de réservations");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  public Fenetre(){
+    this.setSize(1200,800);
+    this.setLocation(0,10);
+    this.setTitle("Recherche de réservations");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panneau = new JPanel();
-        JTextField numReservation = new JTextField("Nom Client");
-	JTextField nomClient = new JTextField("Nom Client");
-	JTextField prenomClient = new JTextField("Prénom Client");
-	JButton rechercherNum = new JButton("Rechercher");
-	JButton rechercherNom = new JButton("Rechercher");
-	JLabel titre = new JLabel("Rechercher une réservation");
-	panneau.setLayout(new GridBagLayout());
-        GridBagConstraints contraintes = new GridBagConstraints();
+    JPanel panneau = new JPanel();
+    JTextField numReservation = new JTextField("Nom Client");
+    JTextField nomClient = new JTextField("Nom Client");
+    JTextField prenomClient = new JTextField("Prénom Client");
+    JButton rechercherNum = new JButton("Rechercher");
+    JButton rechercherNom = new JButton("Rechercher");
+    JLabel titre = new JLabel("Rechercher une réservation");
+    panneau.setLayout(new GridBagLayout());
+    GridBagConstraints contraintes = new GridBagConstraints();
 
-        contraintes.gridx = 1;
-        contraintes.gridy = 1;
-        contraintes.ipady = 15;
-        contraintes.fill = GridBagConstraints.HORIZONTAL;
-        contraintes.anchor = GridBagConstraints.CENTER;
-        contraintes.insets = new Insets(5,400,100,400);
-        contraintes.weightx = 0.1;
-        contraintes.gridwidth = 2;
+    modifierContraintes(contraintes, 1, 1, 15, GridBagConstraints.HORIZONTAL,
+        GridBagConstraints.CENTER, new Insets(5, 400, 100, 400),
+        0.1, 2);
+    panneau.add(numReservation, contraintes);
 
-        panneau.add(numReservation, contraintes);
+    modifierContraintes(contraintes, 1, 2, contraintes.ipady,
+        GridBagConstraints.HORIZONTAL, contraintes.anchor,
+        new Insets(5, 400, 5, 10), contraintes.weightx, 1);
+    panneau.add(nomClient, contraintes);
 
-        contraintes.insets = new Insets(5,400,5,10);
-        contraintes.fill = GridBagConstraints.HORIZONTAL;
-        contraintes.gridwidth = 1;
-        //contraintes.ipady = 15;
-        contraintes.gridx = 1;
-        contraintes.gridy = 2;
+    modifierContraintes(contraintes, 2, 2, contraintes.ipady,
+        GridBagConstraints.HORIZONTAL, contraintes.anchor,
+        new Insets(5, 10, 5, 400), contraintes.weightx,
+        contraintes.gridwidth);
+    panneau.add(prenomClient, contraintes);
 
-	panneau.add(nomClient, contraintes);
+    modifierContraintes(contraintes, 2, 1, contraintes.ipady,
+        GridBagConstraints.HORIZONTAL, contraintes.anchor,
+        contraintes.insets, contraintes.weightx, contraintes.gridwidth);
+    panneau.add(rechercherNum, contraintes);
 
-	contraintes.insets = new Insets(5,10,5,400);
-	contraintes.fill = GridBagConstraints.HORIZONTAL;
-	//contraintes.ipady = 15;
-	contraintes.gridx = 2;
-	contraintes.gridy = 2;
+    modifierContraintes(contraintes, 2, 3, contraintes.ipady,
+        GridBagConstraints.HORIZONTAL, contraintes.anchor,
+        contraintes.insets, contraintes.weightx, contraintes.gridwidth);
+    panneau.add(rechercherNom, contraintes);
 
-	panneau.add(prenomClient, contraintes);
+    modifierContraintes(contraintes, 2, 0, contraintes.ipady,
+        GridBagConstraints.HORIZONTAL, contraintes.anchor,
+        contraintes.insets, contraintes.weightx, contraintes.gridwidth);
+    panneau.add(titre, contraintes);
+    this.add(panneau);
 
-	contraintes.fill = GridBagConstraints.HORIZONTAL;
-	//contraintes.ipady = 15;
-	contraintes.gridx = 2;
-	contraintes.gridy = 1;
+  }
 
-	panneau.add(rechercherNum, contraintes);
-
-	contraintes.fill = GridBagConstraints.HORIZONTAL;
-	//contraintes.ipady = 15;
-	contraintes.gridx = 2;
-	contraintes.gridy = 3;
-
-	panneau.add(rechercherNom, contraintes);
-
-	contraintes.fill = GridBagConstraints.HORIZONTAL;
-	contraintes.gridx = 2;
-	contraintes.gridy = 0;
-		
-	panneau.add(titre, contraintes);
-	this.add(panneau);
-	
-    }
+  public void modifierContraintes(GridBagConstraints c, int gridx, int gridy, int ipady, int fill, int anchor, Insets insets, double weightx, int gridwidth){
+    c.gridx = gridx;
+    c.gridy = gridy;
+    c.ipady = ipady;
+    c.fill = fill;
+    c.anchor = anchor;
+    c.insets = insets;
+    c.weightx = weightx;
+    c.gridwidth = gridwidth;
+  }
 }
-   
+
 
 public class Projet_v1 {
-    public static void main (String[] args) {
-	JFrame f;
-	f = new Fenetre();
-	f.setVisible(true);
-    }
+  public static void main (String[] args) {
+    JFrame f;
+    f = new Fenetre();
+    f.setVisible(true);
+  }
 }
 
 
