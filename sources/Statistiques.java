9314dc0ab8be28a979c538086e5d4cc81f520ad1
romainsim <rom.simon32@gmail.com>
2017-10-20 17:44:38 +0000

    Fin Stats

--- sources/Statistiques.java
+++ sources/Statistiques.java
@@ -1,49 +1,69 @@
-import java.util.Date;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
 public class Statistiques{
-  public Date date;
-  public Date dateDebutPeriode;
-  public Date dateFinPeriode;
-  public int tauxOccupation;
-  public int tauxNonPresentation;
+    public JPanel panneau = new JPanel();
+    int id = 0;
+    public JTextField dateJourOccup = new JTextField();
+    public JLabel resultTauxOccupJour = new JLabel();
+    public JTextField dateJourNonOccup = new JTextField();
+    public JLabel resultTauxNonOccup = new JLabel();
+    public JTextField periodeJourOccup = new JTextField("YYYY-mm-dd YYYY-mm-dd");
+    public JLabel resultTauxOccupPeriode = new JLabel();
+    
+    public Statistiques() {
+	JFrame f = new JFrame();
+	f.setSize(1700,500);
+	f.setLocation(0,10);
+	f.setTitle("Statistiques");
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	f.add(panneau);
+	Stats();
+	f.setVisible(true);
+    }
 
-  public Statistiques(Date newDate, Date newDateDebutPeriode, Date newDateFinPeriode, Chambre[] chambresOccupees){
-    this.date = newDate;
-    this.dateDebutPeriode = newDateDebutPeriode;
-    this.dateFinPeriode = newDateFinPeriode;
-    this.tauxOccupation = chambresOccupees.length;
-    this.tauxNonPresentation = 100 - this.tauxOccupation;
-  }
+    public void Stats() {
+	GridLayout layout = new GridLayout(3,4);
+	layout.setHgap(50);
+	layout.setVgap(50);
+	panneau.setLayout(layout);
 
-  public void setDate(Date newDate){
-    this.date = newDate;
-  }
+	JLabel tauxOccupationJour = new JLabel("Entrez la date pour le taux d'occupation :");
+	panneau.add(tauxOccupationJour);
+	panneau.add(dateJourOccup);
+	JButton tauxOccup = new JButton("Calculez");
+	panneau.add(tauxOccup);
+	panneau.add(resultTauxOccupJour);
 
-  public void setDateDebutPeriode(Date newDate){
-    this.dateDebutPeriode = newDate;
-  }
+	JLabel tauxNonOccupationJour = new JLabel("Entrez la date pour le taux de non presentation :");
+	panneau.add(tauxNonOccupationJour);
+	panneau.add(dateJourNonOccup);
+	JButton tauxNonOccup = new JButton("Calculez");
+	panneau.add(tauxNonOccup);
+	panneau.add(resultTauxNonOccup);
 
-  public void setDateFinPeriode(Date newDate){
-    this.dateFinPeriode = newDate;
-  }
+	JLabel tauxOccupationPeriode = new JLabel("Entrez la periode pour le taux d'occupation :");
+	panneau.add(tauxOccupationPeriode);
+	panneau.add(periodeJourOccup);
+	JButton tauxOccupPeriode = new JButton("Calculez");
+	panneau.add(tauxOccupPeriode);
+	panneau.add(resultTauxOccupPeriode);
 
-  public Date getDate(){
-    return this.date;
-  }
-
-  public Date getDateFinPeriode(){
-    return this.dateFinPeriode;
-  }
-
-  public Date getDateDebutPeriod(){
-    return this.dateDebutPeriode;
-  }
-
-  public int getTauxOccupation(){
-    return this.tauxOccupation;
-  }
-
-  public int getTauxNonPresentation(){
-    return this.tauxNonPresentation;
-  }
+	Calculateur c1 = new Calculateur(1,panneau);
+	Calculateur c2 = new Calculateur(2,panneau);
+	Calculateur c3 = new Calculateur(3,panneau);
+	c1.setStatistiques(this);
+	c2.setStatistiques(this);
+	c3.setStatistiques(this);
+	tauxOccup.addActionListener(c1);
+	tauxNonOccup.addActionListener(c2);
+	tauxOccupPeriode.addActionListener(c3);
+    }
+    
+    public static void main(String[] args) {
+	Statistiques stats = new Statistiques();
+    }
+    
 }
